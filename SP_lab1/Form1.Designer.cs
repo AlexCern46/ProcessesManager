8c82c31b67e3db952127b3229237a3501ae9b8d6
sasha <sasha@DESKTOP-VT96VM3>
2023-09-14 20:08:38 +0300

    Добавьте файлы проекта.

---
+++ SP_lab1/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace SP_lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listViewThreads = new System.Windows.Forms.ListView();
+            this.listViewProcesses = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listViewThreads
+            // 
+            this.listViewThreads.HideSelection = false;
+            this.listViewThreads.Location = new System.Drawing.Point(738, 41);
+            this.listViewThreads.Name = "listViewThreads";
+            this.listViewThreads.Size = new System.Drawing.Size(155, 412);
+            this.listViewThreads.TabIndex = 0;
+            this.listViewThreads.UseCompatibleStateImageBehavior = false;
+            this.listViewThreads.View = System.Windows.Forms.View.Details;
+            // 
+            // listViewProcesses
+            // 
+            this.listViewProcesses.HideSelection = false;
+            this.listViewProcesses.Location = new System.Drawing.Point(12, 41);
+            this.listViewProcesses.Name = "listViewProcesses";
+            this.listViewProcesses.Size = new System.Drawing.Size(720, 412);
+            this.listViewProcesses.TabIndex = 1;
+            this.listViewProcesses.UseCompatibleStateImageBehavior = false;
+            this.listViewProcesses.View = System.Windows.Forms.View.Details;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(180, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "labelTotalProcesses";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 26);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "buttonRefresh";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(905, 462);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listViewProcesses);
+            this.Controls.Add(this.listViewThreads);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listViewThreads;
+        private System.Windows.Forms.ListView listViewProcesses;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ SP_lab1/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Management;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace SP_lab1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            listViewProcesses.SelectedIndexChanged += listViewProcesses_SelectedIndexChanged;
+
+            listViewProcesses.Columns.Add("Process Name", 200);
+            listViewProcesses.Columns.Add("Process ID", 70);
+            listViewProcesses.Columns.Add("Process Owner", 150);
+            listViewProcesses.Columns.Add("Working Set", 80);
+            listViewProcesses.Columns.Add("Base Priority", 70);
+            listViewProcesses.Columns.Add("Thread Count", 80);
+
+            listViewThreads.Columns.Add("Thread ID");
+            listViewThreads.Columns.Add("Base Priority", 70);
+        }
+
+        private void UpdateProcessList()
+        {
+            listViewProcesses.Items.Clear();
+
+            Process[] processes = Process.GetProcesses();
+
+            Parallel.ForEach(processes, (process) =>
+            {
+                ListViewItem item = new ListViewItem(process.ProcessName);
+                item.SubItems.Add(process.Id.ToString());
+                item.SubItems.Add(GetProcessOwner(process.Id));
+                item.SubItems.Add((process.WorkingSet64 / (1024 * 1024)).ToString("N2") + " MB");
+                item.SubItems.Add(process.BasePriority.ToString());
+                item.SubItems.Add(process.Threads.Count.ToString());
+
+                listViewProcesses.Items.Add(item);
+            });
+
+            /*foreach (Process process in processes)
+            {
+                ListViewItem item = new ListViewItem(process.ProcessName);
+                item.SubItems.Add(process.Id.ToString());
+                item.SubItems.Add(GetProcessOwner(process.Id));
+                item.SubItems.Add((process.WorkingSet64 / (1024 * 1024)).ToString("N2") + " MB");
+                item.SubItems.Add(process.BasePriority.ToString());
+                item.SubItems.Add(process.Threads.Count.ToString());
+
+                listViewProcesses.Items.Add(item);
+            }*/
+
+            label1.Text = "Total Processes: " + processes.Length;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            UpdateProcessList();
+        }
+
+        private void listViewProcesses_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listViewProcesses.SelectedItems.Count > 0)
+            {
+                int selectedProcessId = int.Parse(listViewProcesses.SelectedItems[0].SubItems[1].Text);
+                ShowProcessThreads(selectedProcessId);
+            }
+        }
+
+        private void ShowProcessThreads(int processId)
+        {
+            listViewThreads.Items.Clear();
+
+            Process process = Process.GetProcessById(processId);
+            ProcessThreadCollection threads = process.Threads;
+
+            foreach (ProcessThread thread in threads)
+            {
+                ListViewItem item = new ListViewItem(thread.Id.ToString());
+                item.SubItems.Add(thread.Id.ToString());
+                item.SubItems.Add(thread.BasePriority.ToString());
+
+                listViewThreads.Items.Add(item);
+            }
+        }
+
+        private string GetProcessOwner(int processID)
+        {
+            string processOwner = "Unknown owner";
+
+            try
+            {
+                using (ManagementObjectSearcher searcher = new ManagementObjectSearcher($"Select * From Win32_Process Where ProcessId = {processID}"))
+                using (ManagementObjectCollection processList = searcher.Get())
+                {
+                    foreach (ManagementObject mo in processList)
+                    {
+                        string[] ownerInfo = new string[2];
+                        mo.InvokeMethod("GetOwner", (object[])ownerInfo);
+                        processOwner = $"{ownerInfo[0]}\\{ownerInfo[1]}";
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+
+            return processOwner;
+        }
+    }
+}
